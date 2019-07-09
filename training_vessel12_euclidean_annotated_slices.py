49344a99a57aaddeed4c19df31aa92f676b8a516
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-07-09 22:38:46 +0200

    introduce cli arguments training annotated slices vessel12

--- training_vessel12_euclidean_annotated_slices.py
+++ training_vessel12_euclidean_annotated_slices.py
@@ -4,6 +4,9 @@ from lib.process import Trainer, Evaluator
 import matplotlib.pyplot as plt
 import torch
 import numpy as np
+import argparse
+
+
 # CONSTANST
 MODEL_PATH = './u-net-vessel12_annotated_slices.pth'
 EPOCHS = 200
@@ -14,10 +17,10 @@ trainer = Trainer(model=model,dataset=dataset, batch_size=4)
 trainer.load_model(model, MODEL_PATH)
 evaluator = Evaluator(dataset=dataset)
 
-def train():
+def train(lr = 0.001, progress_bar=False):
     loss_all = []
     for _ in range(EPOCHS):
-        loss = trainer.train_epoch(lr=0.1,progress_bar=False)
+        loss = trainer.train_epoch(lr=lr, progress_bar=progress_bar)
         print('loss epoch',np.array(loss).mean())
         loss_all +=loss
         with torch.no_grad():
@@ -31,11 +34,34 @@ def train():
     print('end of training')
     trainer.save_model(MODEL_PATH)
 
-def eval():
-    # print('DCM factor: ' , evaluator.DCM(model))
+def eval(progress_bar = False):
+    # print('DCM factor: ' , evaluator.DCM(model,progress_bar=progress_bar))
     print('plotting one prediction')
     fig = evaluator.plot_prediction(model=model)
     plt.show()
 
-train()
-eval()
+
+
+def process_command_line():
+    """Parse the command line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Machine Learning exercise 5.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-t", "--progressbar", type=int, default=0,
+                        help="progress bar continuous")
+    parser.add_argument("-lr", "--lr", type=float, default=0.001,
+                        help="learning rate")
+    parser.add_argument("-g", "--epochs", type=int, default=10,
+                        help="parameter gamam of the gaussians")
+
+    return parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    args = process_command_line()
+    global EPOCHS
+    EPOCHS = args.epochs
+    train(lr=args.lr, progress_bar=args.progressbar)
+
+
