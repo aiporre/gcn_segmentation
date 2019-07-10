a7bf62692250b68c9996672f2b8c71ff86578456
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-07-10 15:17:26 +0200

    typo fig saveing

--- lib/utils/savefigs.py
+++ lib/utils/savefigs.py
@@ -3,4 +3,4 @@ import os
 def savefigs(fig_name, fig_dir, fig):
     if not os.path.exists(fig_dir):
         os.makedirs(fig_dir)
-    fig.savefig(os.path(fig_dir,str(fig_name) + '.png'), format='png')
+    fig.savefig(os.path.join(fig_dir,str(fig_name) + '.png'), format='png')
