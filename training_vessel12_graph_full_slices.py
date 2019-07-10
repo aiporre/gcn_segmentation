3e18a025b3c37c1927a8ded30b1101935965c79f
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-07-10 18:48:10 +0200

    fix new notation in script training gfcn

--- training_vessel12_graph_full_slices.py
+++ training_vessel12_graph_full_slices.py
@@ -6,6 +6,7 @@ from lib.process import Trainer, Evaluator
 import matplotlib.pyplot as plt
 import torch
 from config import VESSEL_DIR
+from lib.utils import savefigs
 
 
 
@@ -22,7 +23,8 @@ def process_command_line():
                         help="parameter gamam of the gaussians")
     parser.add_argument("-d", "--vesseldir", type=str, default=VESSEL_DIR,
                         help="parameter gamam of the gaussians")
-
+    parser.add_argument("-f", "--figdir", type=str, default='./fig',
+                        help="path to save figs")
     return parser.parse_args()
 
 # CONSTANST
@@ -37,21 +39,22 @@ trainer = Trainer(model=model,dataset=dataset, batch_size=4)
 trainer.load_model(model, MODEL_PATH)
 evaluator = Evaluator(dataset=dataset)
 
-def train():
+def train(lr=0.001, progress_bar=False):
     for _ in range(EPOCHS):
-        loss = trainer.train_epoch()
+        loss = trainer.train_epoch(lr=lr, progress_bar=progress_bar)
         print('loss',loss)
         with torch.no_grad():
-            score = evaluator.DCM()
+            score = evaluator.DCM(model, progress_bar=progress_bar)
             print('DCM score:', score)
     print('end of training')
     trainer.save_model(MODEL_PATH)
 
-def eval():
-    # print('DCM factor: ' , evaluator.DCM(model))
+def eval(lr=0.001, progress_bar=False, fig_dir='./fig'):
+    # print('DCM factor: ' , evaluator.DCM(model, progress_bar=progress_bar))
     print('plotting one prediction')
     fig = evaluator.plot_prediction(model=model)
+    savefigs(fig_name='gfcn_e{}_lr{}_annotatedslices'.format(EPOCHS, lr),fig_dir=fig_dir, fig=fig)
     plt.show()
 
 train(lr=args.lr, progress_bar=args.progressbar)
-eval(progress_bar=args.progressbar)
+eval(lr=args.lr, progress_bar=args.progressbar, fig_dir=args.figdir)
