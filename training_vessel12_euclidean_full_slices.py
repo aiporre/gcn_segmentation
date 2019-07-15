b49e0347a6054f0c4f2b07981f4994ceb171992c
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-07-15 15:59:57 +0200

    fix model to device

--- training_vessel12_euclidean_full_slices.py
+++ training_vessel12_euclidean_full_slices.py
@@ -41,6 +41,7 @@ dataset = VESSEL12(data_dir=args.vesseldir)
 
 model = UNet(n_channels=1, n_classes=1)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
 
 trainer = Trainer(model=model,dataset=dataset, batch_size=BATCH, device=device)
 trainer.load_model(model, MODEL_PATH)
