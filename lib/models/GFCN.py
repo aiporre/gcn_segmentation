fix DCS calculation

lib/models/GFCN.py
@@ -478,5 +478,5 @@ class GFCN(torch.nn.Module):
         data = recover_grid_barycentric(data, weights=weights1, pos=pos1, edge_index=edge_index1, cluster=cluster1,
                                         batch=batch1, transform=T.Cartesian(cat=False))
 
-        # return F.sigmoid(data.x)
-        return x
+        return F.sigmoid(data.x)
+        # return x

lib/process/evaluation.py
@@ -6,12 +6,13 @@ from lib.utils import print_debug
 from torch import sigmoid
 import numpy as np
 class Evaluator(object):
-    def __init__(self, dataset, batch_size=64, to_tensor=True, device=None):
+    def __init__(self, dataset, batch_size=64, to_tensor=True, device=None, sigmoid=False):
         self.dataset = dataset.test
         self._batch_size = batch_size
         self.dataset.enforce_batch(self._batch_size)
         self.to_tensor = to_tensor
         self.device = device if device is not None else torch.device('cpu')
+        self.sigmoid = sigmoid
 
 
     def DCM(self, model, progress_bar=True):
@@ -27,7 +28,7 @@ class Evaluator(object):
             features = features.to(self.device)
             label = label.to(self.device)
             prediction = model(features)
-            pred_mask = (prediction > 0.5).float()
+            pred_mask = (sigmoid(prediction) > 0.5).float() if self.sigmoid else (prediction > 0.5).float()
             # reorganize prediction according to the batch.
             if not pred_mask.size(0) == label.size(0):
                 b = label.size(0)
@@ -62,8 +63,8 @@ class Evaluator(object):
             # to device
             features = features.to(self.device)
             label = label.long().to(self.device)
-
-            pred = model(features).max(1)[1]
+            prediction = model(features)
+            pred_mask = (sigmoid(prediction) > 0.5).float() if self.sigmoid else (prediction > 0.5).float()
             if not pred.size(0) == label.size(0):
                 b = label.size(0)
                 pred = pred.view(b, -1)
@@ -100,7 +101,7 @@ class Evaluator(object):
         input = input.to(self.device)
         prediction = model(input)
         # pred_mask = (sigmoid(prediction) > 0.5).float()
-        pred_mask = (prediction > 0.5).float()
+        pred_mask = (sigmoid(prediction) > 0.5).float() if self.sigmoid else (prediction > 0.5).float()
 
         if not isinstance(image,np.ndarray):
             dimension = image.x.size(0)# it will assume a square image, though we need a transformer for that

training.py
@@ -53,7 +53,7 @@ def process_command_line():
 
 args = process_command_line()
 EPOCHS = args.epochs
-MODEL_PATH = './{}-vessel12-full_slices.pth'.format(args.net)
+MODEL_PATH = './{}-ds{}.pth'.format(args.net, args.dataset)
 EPOCHS = args.epochs
 BATCH = args.batch
 
@@ -92,18 +92,20 @@ if args.criterion == 'BCE':
     criterion = nn.BCELoss()
 elif args.criterion == 'BCElogistic':
     criterion = nn.BCEWithLogitsLoss()
+    sigmoid = True
 else:
     criterion = nn.BCELoss()
+    sigmoid = False
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = model.to(device)
 if args.dataset[0] == 'G':
     trainer = Trainer(model=model,dataset=dataset, batch_size=BATCH,to_tensor=False, device=device)
-    evaluator = Evaluator(dataset=dataset, batch_size=BATCH, to_tensor=False, device=device)
+    evaluator = Evaluator(dataset=dataset, batch_size=BATCH, to_tensor=False, device=device, sigmoid=sigmoid)
     trainer.load_model(model, MODEL_PATH)
 else:
     trainer = Trainer(model=model, dataset=dataset, batch_size=BATCH, device=device)
-    evaluator = Evaluator(dataset=dataset, batch_size=BATCH, device=device)
+    evaluator = Evaluator(dataset=dataset, batch_size=BATCH, device=device, sigmoid=sigmoid)
     trainer.load_model(model, MODEL_PATH)
 
 
@@ -115,12 +117,12 @@ def train(lr=0.001, progress_bar=False, fig_dir='./figs',prefix='NET'):
     P = []
     R = []
     for _ in range(EPOCHS):
-        model.train()
-        loss = trainer.train_epoch(lr=lr, progress_bar=progress_bar)
-        mean_loss = np.array(loss).mean()
-        loss_epoch.append(mean_loss)
-        print('loss epoch', mean_loss)
-        loss_all += loss
+        # model.train()
+        # loss = trainer.train_epoch(lr=lr, progress_bar=progress_bar)
+        # mean_loss = np.array(loss).mean()
+        loss_epoch.append(0)#mean_loss)
+        # print('loss epoch', mean_loss)
+        loss_all += [1]#,loss
         with torch.no_grad():
             model.eval()
             DCS.append(evaluator.DCM(model, progress_bar=progress_bar))
@@ -130,17 +132,22 @@ def train(lr=0.001, progress_bar=False, fig_dir='./figs',prefix='NET'):
             R.append(r)
             print('DCS score:', DCS[-1], 'accuracy ', a, 'precision', p, 'recall', r )
     fig = plt.figure(figsize=(10,10))
-    plt.subplot(1,3,1)
+    loss_all = np.array(loss_all)
+    measurements = np.array([DCS,P,A,R])
+    np.save('{}_e{}_lr{}_ds{}_lossall'.format(prefix, EPOCHS, lr, args.dataset), measurements)
+    np.save('{}_e{}_lr{}_ds{}_measurements'.format(prefix, EPOCHS, lr, args.dataset),measurements)
+
+    plt.subplot(3,1,1)
     plt.plot(loss_all)
     plt.xlabel('iterations')
     plt.ylabel('loss')
     plt.title('loss history')
-    plt.subplot(1, 3, 2)
+    plt.subplot(3,1,2)
     plt.plot(loss_epoch)
     plt.xlabel('epochs')
     plt.ylabel('loss')
     plt.title('loss history')
-    plt.subplot(1, 3, 3)
+    plt.subplot(3,1,3)
     plt.plot(DCS)
     plt.plot(P)
     plt.plot(A)