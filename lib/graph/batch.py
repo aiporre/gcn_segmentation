9f42e27f0fc559eccf3230c28a73a973cbb609d3
Ariel <ariel.iporre.rivas@gmail.com>
2021-10-08 18:45:12 +0200

    fix inconsistency on shape while createing batch in the case of acc calcualtion

--- lib/graph/batch.py
+++ lib/graph/batch.py
@@ -2,5 +2,5 @@ import torch
 
 
 def to_torch_batch(batch):
-    batch = torch.stack([batch.x[batch.batch == n] for n in range(batch.num_graphs)],axis=0)
+    batch = torch.stack([batch.x[batch.batch == n].squeeze() for n in range(batch.num_graphs)],axis=0)
     return batch
